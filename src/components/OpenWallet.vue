34b300967f9eafc62b5b4b700e667ac3422e86d0
William Dens <denswilliam@hotmail.com>
2017-11-19 12:08:34 +0100

    Wallet opening with QR code scanner

--- src/components/OpenWallet.vue
+++ src/components/OpenWallet.vue
@@ -9,34 +9,54 @@
           <div class="field">
             <input v-model="passphrase" class="input" type="text" placeholder="Enter passphrase">
           </div>
+          <!-- Open with input -->
           <button @click.prevent="openWallet()" class="ui button green"
                   :class="{ 'disabled': passphrase === '' }">
             <i class="fa fa-unlock"></i>
             Open
           </button>
+          <!-- Camera toggle -->
+          <button v-if="!cameraRunning" @click.prevent="startCamera()" class="ui button green">
+            <i class="fa fa-qrcode"></i>
+            Scan
+          </button>
+          <button v-if="cameraRunning" @click.prevent="stopCamera()" class="ui button red">
+            <i class="fa fa-qrcode"></i>
+            Stop
+          </button>
+          <!-- New -->
           <button @click.prevent="newWallet()" class="ui button teal basic">
             <i class="fa fa-plus"></i>
             New
           </button>
         </form>
       </div>
+      <video id="preview"></video>
     </div>
   </div>
 </template>
 
 <script>
 import * as jark from 'jark'
+import Instascan from 'instascan'
 
 export default {
   name: 'openWallet',
   data () {
     return {
+      scanner: null,
       passphrase: ''
+    }
+  },
+  computed: {
+    cameraRunning () {
+      return (this.scanner != null)
     }
   },
   methods: {
     openWallet () {
       if (this.passphrase === '') return null
+      console.log('Opening wallet')
       const keys = jark.getKeys(this.passphrase)
       if (keys.address) {
         this.$store.dispatch('openWallet', keys.address)
@@ -46,9 +66,32 @@ export default {
     },
     newWallet () {
       this.$router.push({ name: 'NewWallet' })
+    },
+    startCamera () {
+      this.scanner = new Instascan.Scanner({ video: document.getElementById('preview') })
+      this.scanner.addListener('scan', (content) => {
+        this.passphrase = content
+        this.openWallet()
+      })
+      Instascan.Camera.getCameras().then((cameras) => {
+        if (cameras.length > 0) {
+          this.scanner.start(cameras[0])
+        } else {
+          console.error('No cameras found.')
+        }
+      }).catch(function (e) {
+        console.error(e)
+      })
+    },
+    stopCamera () {
+      this.scanner.stop()
+      this.scanner = null
     }
   },
   mounted () {
+  },
+  beforeDestroy () {
+    if (this.scanner != null) this.stopCamera()
   }
 }
 </script>
