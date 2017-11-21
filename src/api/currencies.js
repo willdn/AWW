48c5a1cf634d20b0805dc324126ba85647a8edb3
William Dens <denswilliam@hotmail.com>
2017-11-22 00:05:57 +0100

    Add currency

--- src/api/currencies.js
+++ src/api/currencies.js
@@ -2,17 +2,21 @@
  * @property {Object} currencies - List of fiat currencies
  */
 const currencies = {
+  BTC: {
+    id: 'BTC',
+    label: 'Ƀ'
+  },
   EUR: {
     id: 'EUR',
     label: '€'
   },
+  GBP: {
+    id: 'GBP',
+    label: '£'
+  },
   USD: {
     id: 'USD',
     label: '$'
-  },
-  BTC: {
-    id: 'BTC',
-    label: 'Ƀ'
   }
 }
 
