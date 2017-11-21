0cdd8cd60b41b1f347b86ec05e0904d95680cfe7
William Dens <denswilliam@hotmail.com>
2017-11-21 23:48:06 +0100

    Raise segment OpenWallet/Main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,7 +3,7 @@
     <div class="ui segment basic center aligned">
       <!-- Ark logo -->
       <img class="ui centered image tiny" :src="require('../assets/img/ark-logo.png')">
-      <div class="ui segment center aligned">
+      <div class="ui segment raised center aligned">
         <div class="ui two stackable cards">
           <router-link :to="{ name: 'NewWallet'}" class="card teal raised">
             <div class="content">
@@ -37,6 +37,10 @@ export default {
   margin-top: 0.35rem;
   margin-bottom: 0.15rem;
   color: rgba(0,0,0,.87);
+}
+.cards {
+    margin-top: 0.15em;
+    margin-bottom: 0.1em;
 }
 .container{
   display: flex; 
--- src/components/OpenWallet.vue
+++ src/components/OpenWallet.vue
@@ -3,7 +3,7 @@
     <div class="ui segment basic center aligned">
       <!-- Ark logo -->
       <img class="ui centered image tiny" :src="require('../assets/img/ark-logo.png')">
-      <div class="ui segment center aligned">
+      <div class="ui segment raised center aligned">
         <!-- Login options -->
         <div class="ui four stackable cards">
           <a class="raised card" @click.prevent="passphrasePanelVisible = !passphrasePanelVisible"
@@ -126,6 +126,10 @@ export default {
   margin-top: 0.35rem;
   margin-bottom: 0.15rem;
   color: rgba(0,0,0,.87);
+}
+.cards {
+    margin-top: 0.15em;
+    margin-bottom: 0.15em;
 }
 .container{
   display: flex; 
