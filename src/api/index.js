f67b822011590d2b33cbcccf2e0880e3974f55aa
William Dens <denswilliam@hotmail.com>
2017-12-04 23:41:33 +0100

    Remove harcoded send transaction endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,4 @@
-import store from '../store'
-
 /**
  * @property {string} donationAddress - Donation address
  */
 export const donationAddress = 'AePNZAAtWhLsGFLXtztGLAPnKm98VVC8tJ'
-
-/**
- * Return API endpoint
- * @return {string} Endpoint
- */
-export const getEndpoint = () => {
-  if (store.getters.networkType.label === 'Main') return 'https://node1.arknet.cloud'
-  if (store.getters.networkType.label === 'Dev') return 'https://dev.arkcoin.net'
-}
