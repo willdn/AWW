52efe119f5383c2e9ab9b03846a4607a3ae12e86
William Dens <denswilliam@hotmail.com>
2017-12-05 11:19:31 +0100

    Get ark market in API

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,21 @@
+import axios from 'axios'
+
 /**
  * @property {string} donationAddress - Donation address
  */
 export const donationAddress = 'AePNZAAtWhLsGFLXtztGLAPnKm98VVC8tJ'
+
+/**
+ * Get ARK market from cryptocompare
+ * @param {string} - Fiat id currency to fetch
+ * @return {Promise<Object>} Current fiat price
+ */
+export const getARKMarket = (fiatCurrencyId) => {
+  return axios.get(`https://min-api.cryptocompare.com/data/price?fsym=ARK&tsyms=${fiatCurrencyId}`)
+  .then((res) => {
+    return res
+  })
+  .catch((err) => {
+    if (err) console.log(err)
+  })
+}
